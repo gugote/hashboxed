708f340ae6d164898ed7f5bec4a814768ed3a415
Carlos Bruscoli <cb@hashboxed.com>
2022-05-03 18:16:08 -0300

    new site before dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/main.scss'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Hashboxed</title>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
