ee5de1f793f52b6cd6ffd1ebb4a801b3c2ddd97f
Carlos Bruscoli <cb@hashboxed.com>
2022-12-20 11:47:30 -0300

    meh

--- pages/daily-ui-challenge.js
+++ pages/daily-ui-challenge.js
@@ -3,6 +3,10 @@ import SwitchBox from "../components/switchbox";
 import Footer from "../components/footer";
 import Link from "next/link";
 
+// IMAGES
+import Image from "next/image";
+import thumb01 from "../public/images/daily-ui-challenge/01-sign-up-thumb.png";
+
 export default function dailyUiChallenge(){
   return (
     <div className='content'>
@@ -14,7 +18,12 @@ export default function dailyUiChallenge(){
         <div className='cards-container cf'>
           <Link href="/daily-ui-challenge/01" legacyBehavior>
             <div className='card'>
-              <div className='card-image'></div>
+              <div className='card-image'>
+                <Image 
+                  src={thumb01},
+                  alt="#01 - SIGN UP"
+                />
+              </div>
               <div className="card-copy">
                 <span>#01</span>
                 <h3>Sign Up</h3>
