68061a508e1c7ff325250a34446b9960f22778e8
Carlos Bruscoli <cb@hashboxed.com>
2024-03-12 19:10:02 -0300

    Fixed deploy character issues

--- pages/case-studies.js
+++ pages/case-studies.js
@@ -22,7 +22,7 @@ export default function CaseStudies() {
           <h2>Ring.com</h2>
           <h3 className="title">INTERNAL DASHBOARD</h3>
           <h4>Internal dashboard for devices administration</h4>
-          <p><strong>Key Aspects of the Project:</strong> This application empowers Ring customer support agents to deliver enhanced support to users via a user-friendly dashboard. The core objective of the project was to minimize the duration of each customer call. To achieve this, the design focuses on creating easily navigable screens and providing swift access to various sections, contrasting with the previous app's approach of consolidating all information into a single, extensive scrollable screen.</p>
+          <p><strong>Key Aspects of the Project:</strong> This application empowers Ring customer support agents to deliver enhanced support to users via a user-friendly dashboard. The core objective of the project was to minimize the duration of each customer call. To achieve this, the design focuses on creating easily navigable screens and providing swift access to various sections, contrasting with the previous app&apos;s approach of consolidating all information into a single, extensive scrollable screen.</p>
           <ul className="skills-used">
             <li>UX/UI Design of the dashboard</li>
             <li>Frontend Code: Vue.js with Element+ for styling base of some components like forms</li>
@@ -51,12 +51,12 @@ export default function CaseStudies() {
               height={145}
             />
           </div>
-          <small>* Can't show detailed samples since it's an internal tool protected by NDA but happy to discuss technical details and other projects @ Ring over a call.</small>
+          <small>* Can&apos;t show detailed samples since it&apos;s an internal tool protected by NDA but happy to discuss technical details and other projects @ Ring over a call.</small>
         </div>
         <div className="case-study no-link">
           <h2>Hashboxed</h2>
           <h3 className="title">SESSIONS APP</h3>
-          <h4>Designed specifically for professionals who manage their work through sessions and appointments. This app empowers the doctor to effortlessly monitor each patient's progress and the number of sessions remaining.</h4> 
+          <h4>Designed specifically for professionals who manage their work through sessions and appointments. This app empowers the doctor to effortlessly monitor each patient&apos;s progress and the number of sessions remaining.</h4> 
           <p>Featuring a fully customizable calendar tailored to their unique schedule, it also provides a daily digest for an efficient overview of their day. Additionally, it offers instant access to patient data at a glance, ensuring the professional have all the information when they need it. This tool is the ultimate companion for maintaining control and enhancing the management of patient sessions and appointments.</p>
           <ul className="skills-used">
             <li>Product Design</li>
--- pages/me.js
+++ pages/me.js
@@ -15,19 +15,19 @@ export default function Me() {
         <SwitchBox></SwitchBox>
         <h1 className='hero hero-button'>About Me</h1>
         <h2>Introduction</h2>
-        <p>Ever since I kicked off my professional journey, I've dived into a whole bunch of cool projects. I'm talking websites, intricate web apps, mobile apps (both the native kind and those optimized for iOS and Android), e-commerce platforms, digital newspapers, even the .nic registry in Argentina, and some wild fingerprint UI projects for Windows. A lot of this stuff is under wraps due to a bunch of NDAs.</p>
+        <p>Ever since I kicked off my professional journey, I&apos;ve dived into a whole bunch of cool projects. I&apos;m talking websites, intricate web apps, mobile apps (both the native kind and those optimized for iOS and Android), e-commerce platforms, digital newspapers, even the .nic registry in Argentina, and some wild fingerprint UI projects for Windows. A lot of this stuff is under wraps due to a bunch of NDAs.</p>
         <h2>Work Highlights</h2>
         <h3 className='title'>Devego</h3>
         <small>2007 - 2011</small>
         <p>Co-founded Devego, a bespoke software development company, serving international clients, where working as a Product Designer / Art Director managed diverse projects, significantly enhancing business growth before successfully exiting.</p>
         <h3 className='title'>Hashboxed</h3>
         <small>2011 - Today</small>
-        <p>Established Hashboxed, providing UI/UX design and frontend development services to startups globally, where I directed projects across E-commerce, digital newspapers, and unique UI solutions for Windows. CMS's, SPA, Landing Pages, Blogs, Full Fledge websites, you name it we probably did it.</p>
+        <p>Established Hashboxed, providing UI/UX design and frontend development services to startups globally, where I directed projects across E-commerce, digital newspapers, and unique UI solutions for Windows. CMS&apos;s, SPA, Landing Pages, Blogs, Full Fledge websites, you name it we probably did it.</p>
         <h3 className='title'>Ring.com</h3>
         <small>2014 - Today</small>
-        <p>In 2014 I joined <Link href="http://ring.com/" target="_blank" rel="noopener">Ring.com</Link> (employee number 10), an IoT company which was acquired by Amazon in 2018. I was hired initially as a UI/UX designer and worked on the first design of the mobile app, first version of the website, creation of a styleguide, internal admin tools and billing/user accounts for 4 years. My role evolved and today I am a Frontend Developer / Design Engineer for all the Ecommerce portion of the site, working alongside the Design Team to help them reach the design interaction they want without forcing or compromising the code by doing crazy stuff to achieve those objectives and focusing on UX and UI improvements.</p>
+        <p>In 2014 I joined <Link href="http://ring.com/" target="_blank" rel="noopener">Ring.com</Link> (employee number 10), an IoT company which was acquired by Amazon in 2018. I was hired initially as a UI/UX designer and worked on the first design of the mobile app, first version of the website, creation of a styleguide, ecommerce and billing/user accounts for more than 4 years. My role evolved and today I am a Frontend Developer / Design Engineer for the Internal Admin portion of the site, working alongside the Design Team to help them reach the design interaction they want without forcing or compromising the code by doing crazy stuff to achieve those objectives and focusing on UX and UI improvements.</p>
         <h2>Personal Info</h2>
-        <p>I live in Formosa, a small town in the nort of Argentina, father to three incredible kids and the proud owner of Zelda, the grumpiest puppy you'll ever meet, and Whisky, our cat who's convinced she's the real boss of the house.</p>
+        <p>I live in Formosa, a small town in the nort of Argentina, father to three incredible kids and the proud owner of Zelda, the grumpiest puppy you&apos;ll ever meet, and Whisky, our cat who&apos;s convinced she&apos;s the real boss of the house.</p>
         <p>About my language, I can speak/write fluent English and Spanish.</p>
       </section>
         <Footer></Footer>
