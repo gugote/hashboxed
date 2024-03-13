3914a3b174efcefa29d3b9a18bd792cb32e5f412
Carlos Bruscoli <cb@hashboxed.com>
2024-03-13 15:41:20 -0300

    Added dondedonar case study

--- pages/case-studies.js
+++ pages/case-studies.js
@@ -8,6 +8,7 @@ import plc02 from "../public/images/case-studies/ring/plc-app-02.png";
 import plc03 from "../public/images/case-studies/ring/plc-app-03.png";
 import sessions from "../public/images/case-studies/sessions/sessions.png";
 import arcosule from "../public/images/case-studies/arcosule/arcosule.png";
+import dondedonar from "../public/images/case-studies/dondedonar/dondedonar.png";
 
 export default function CaseStudies() {
   return (
@@ -21,7 +22,7 @@ export default function CaseStudies() {
         <h1 className='hero hero-button'>Case Studies</h1>
         <div className="case-study no-link">
           <h2>Ring.com</h2>
-          <h3 className="title">INTERNAL DASHBOARD</h3>
+          <h3 className="title">INTERNAL DASHBOARD - 2024</h3>
           <h4>Internal dashboard for devices administration</h4>
           <p>This application empowers Ring customer support agents to deliver enhanced support to users via a user-friendly dashboard. The core objective of the project was to minimize the duration of each customer call. To achieve this, the design focuses on creating easily navigable screens and providing swift access to various sections, contrasting with the previous app&apos;s approach of consolidating all information into a single, extensive scrollable screen.</p>
           <ul className="skills-used">
@@ -56,7 +57,7 @@ export default function CaseStudies() {
         </div>
         <div className="case-study no-link">
           <h2>Hashboxed</h2>
-          <h3 className="title">SESSIONS APP</h3>
+          <h3 className="title">SESSIONS APP - 2024 - NEW</h3>
           <h4>Designed specifically for professionals who manage their work through sessions and appointments. This app empowers the doctor to effortlessly monitor each patient&apos;s progress and the number of sessions remaining.</h4> 
           <p>Featuring a fully customizable calendar tailored to their unique schedule, it also provides a daily digest for an efficient overview of their day. Additionally, it offers instant access to patient data at a glance, ensuring the professional have all the information when they need it. This tool is the ultimate companion for maintaining control and enhancing the management of patient sessions and appointments.</p>
           <ul className="skills-used">
@@ -80,7 +81,29 @@ export default function CaseStudies() {
         </div>
         <div className="case-study no-link">
           <h2>Hashboxed</h2>
-          <h3 className="title">ARCOSULE</h3>
+          <h3 className="title">DONDEDONAR - 2024 - NEW</h3>
+          <h4>We are currently developing an innovative web app designed to bridge the gap between individuals looking to donate items (such as clothing and food) and nearby charity institutions.</h4> 
+          <p>With an intuitive and user-friendly design, users can swiftly locate charitable organizations in their area, accessing essential details like location, types of donations accepted, and options for direct contact through phone calls or text messages. The aim of this project, still in its development phase, is to streamline the process of finding and collaborating with charity institutions, ensuring that support reaches those in need efficiently and directly.</p>
+          <ul className="skills-used">
+            <li>Project Management</li>
+            <li>UX/UI Design of the dashboard</li>
+          </ul>
+          <div className="case-box-big-with-link">
+            <Link href="https://www.figma.com/file/ol57QLjqnPBSgliLJZZTHJ/Donde-Donar?type=design&node-id=0%3A1&mode=design&t=7dirUo0VyZY9lXOV-1" target="_blank" rel="noopener">
+              <Image 
+                src={dondedonar}
+                alt="Donde Donar"
+                width={660}
+                height={300}
+              />
+              <span></span>
+              <small>View Full Figma Project</small>
+            </Link>
+          </div>
+        </div>
+        <div className="case-study no-link">
+          <h2>Hashboxed</h2>
+          <h3 className="title">ARCOSULE - 2023</h3>
           <h4>In designing the landing page for the Arcosule, my focus was on encapsulating the essence of their cutting-edge technology and expertise within a visually appealing digital storefront.</h4> 
           <p>Adopting a modern and clear design direction, the landing page aimed to reflect the innovative spirit of the company while prioritizing an intuitive user experience. Emphasizing readability and seamless navigation through a minimalist aesthetic, the design ensured the company&apos;s solutions and services were the focal point. Crucially, support for responsive design was introduced organically into the project, guaranteeing that the user experience remained consistent and engaging across all devices, an essential consideration for the tech-savvy audience targeted by the company.</p>
           <ul className="skills-used">
