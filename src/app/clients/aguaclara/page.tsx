33b9ea5b62dbc3003d65f2ddec01e5f5d6674a9f
Carlos Bruscoli <cb@hashboxed.com>
2025-01-22 17:59:09 -0300

    Added clients directory with the aguaclara project

---
+++ src/app/clients/aguaclara/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function AguaClara() {
+  return (
+    <div className="mx-auto block w-[1440px]">
+      <Image 
+        alt="Agua Clara"
+        src="/clients/aguaclara/agua-clara-landing.png"
+        width="1440"
+        height="4664"
+      />
+    </div>
+  )
+}
