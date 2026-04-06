26da7498247400438476076b19cae4dc4a809059
Carlos Bruscoli <carlosbruscoli@gmail.com>
2026-04-06 11:18:16 -0300

    Added homepage for ediciones manantial

---
+++ src/app/clients/emanantial/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function EManantial() {
+  return (
+    <div className="mx-auto block w-[1440px]">
+      <Image 
+        alt="Ediciones Manantial"
+        src="/clients/emanantial/manantial-home.png"
+        width="1440"
+        height="4664"
+      />
+    </div>
+  )
+}
